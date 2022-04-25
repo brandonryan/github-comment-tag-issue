ea9946919f0f92539ebe24dacebcd61b88441c4a
Brandon Ryan <bjryan19@protonmail.com>
2022-04-25 16:33:46 -0500

    changed comment to differ from main

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {} from "@actions/core"
 import {context} from "@actions/github"
 
-//TODO: figure out a way to get here
+//TODO: figure out a way to get here v2
 console.dir(context, {depth: null, colors: false})
